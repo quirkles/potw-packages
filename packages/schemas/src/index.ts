98b2d2e60c2eb2d1ee1fe1c78e27b08ec094f941
quirkles <al.quirk@gmail.com>
2024-12-23 21:32:15 -0500

    chore(release): publish
    - project: permissions 3.0.9
    
    - project: type-utils 1.0.7
    
    - project: schemas 1.0.9
    
    - project: utils 0.0.6

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,5 +1,6 @@
+export * from "./lib/contract";
 export * from "./lib/firebase";
-export * from "./lib/sql";
 export * from "./lib/shared";
+export * from "./lib/sql";
 
 export * from "./lib/transforms";
